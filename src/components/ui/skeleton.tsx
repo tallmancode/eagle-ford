421c8b8febf1fe88a3b3ff8d946c1d2300dd8c79
Steve (TallmanCode) Stewart <steve@tallmancode.co.za>
2026-08-06 12:15:52 +0200

    chore: drop unrelated specials WIP from seed removal PR

--- src/components/ui/skeleton.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/lib/utils/cn'
-import * as React from 'react'
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />
-}
-
-export { Skeleton }
